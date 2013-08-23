e9b8b821c6041f60cb0bb7d5b799d2920b76ab93
Rémy <remy@colorz.fr>
2013-08-23 18:08:03 +0200

    4

--- scripts.js
+++ scripts.js
@@ -7,27 +7,42 @@ window.addEvent('domready', function(j) {
     var map;
 
     // locations
-    var brooklyn = new google.maps.LatLng(40.6743890, -73.9455);
-    var chicago = new google.maps.LatLng(41.850033, -87.6500523);
-
     var vancouver = new google.maps.LatLng(49.290504,-123.117655);
+    var jasper = new google.maps.LatLng(52.88231,-118.07880);
     var richmond = new google.maps.LatLng(49.19168,-123.11442);
 
+    // waypoints limités à 8
     var steps = [
+        // vancouver = [49.290504,-123.117655],
         vancouverBeaverLake = [49.30520,-123.13878],
-        tsawwassen = [49.00659,-123.13288],
-        victoria = [48.42373,-123.35449],
-        tofino = [49.15243,-125.90249],
-        nanaimo = [49.16664,-123.93497],
-        horseshoeBay = [49.37579,-123.27160],
-        whistler = [50.11632,-122.95736],
-        clearwater = [51.64102,-120.02728]
+        // tsawwassen          = [49.00659,-123.13288],
+        victoria            = [48.42373,-123.35449],
+        tofino              = [49.15243,-125.90249],
+        // nanaimo             = [49.16664,-123.93497],
+        // horseshoeBay        = [49.37579,-123.27160],
+        whistler            = [50.11632,-122.95736],
+        clearwater          = [51.64102,-120.02728]
     ];
-
+    var steps2 = [
+        // jasper              = [52.882,-118.07880],
+        banff               = [51.17836,-115.57077],
+        calgary             = [51.04532,-114.05810],
+        revelstoke          = [50.99867,-118.19439],
+        kelowna             = [49.88795,-119.49601],
+        osoyoos             = [49.02947,-119.45171]
+        // richmond            = [49.19168,-123.11442]
+    ];
 
     var waypts = [];
     steps.each(function(el) {
         waypts.push({
+            location: new google.maps.LatLng(el[0],el[1]),
+            stopover:false
+        });
+    });
+    var waypts2 = [];
+    steps2.each(function(el) {
+        waypts2.push({
             location: new google.maps.LatLng(el[0],el[1]),
             stopover:false
         });
@@ -61,7 +76,6 @@ window.addEvent('domready', function(j) {
             }
         ];
 
-        directionsDisplay = new google.maps.DirectionsRenderer();
 
         var mapOptions = {
             zoom: 9,
@@ -79,11 +93,12 @@ window.addEvent('domready', function(j) {
 
 
         //Directions
+        directionsDisplay = new google.maps.DirectionsRenderer();
         directionsDisplay.setMap(map);
 
         var request = {
             origin:vancouver,
-            destination:richmond,
+            destination:jasper,
             waypoints: waypts,
             optimizeWaypoints: true,
             travelMode: google.maps.DirectionsTravelMode.DRIVING
@@ -91,6 +106,22 @@ window.addEvent('domready', function(j) {
         directionsService.route(request, function(response, status) {
             if (status == google.maps.DirectionsStatus.OK) {
                 directionsDisplay.setDirections(response);
+            }
+        });
+
+        directionsDisplay2 = new google.maps.DirectionsRenderer();
+        directionsDisplay2.setMap(map);
+
+        var request2 = {
+            origin:jasper,
+            destination:richmond,
+            waypoints: waypts2,
+            optimizeWaypoints: true,
+            travelMode: google.maps.DirectionsTravelMode.DRIVING
+        };
+        directionsService.route(request2, function(response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay2.setDirections(response);
             }
         });
 
