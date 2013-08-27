3891c423654baad0ed7e9f6efa77bec5b2038907
Rémy <remy@colorz.fr>
2013-08-27 18:25:10 +0200

    6

--- scripts.js
+++ scripts.js
@@ -1,25 +1,27 @@
+
+var geoCities = {
+    vancouver           : [49.290504,-123.117655],
+    vancouverBeaverLake : [49.30520,-123.13878],
+    tsawwassen          : [49.00659,-123.13288],
+    victoria            : [48.42373,-123.35449],
+    tofino              : [49.15243,-125.90249],
+    nanaimo             : [49.16664,-123.93497],
+    horseshoeBay        : [49.37579,-123.27160],
+    whistler            : [50.11632,-122.95736],
+    clearwater          : [51.64102,-120.02728],
+    jasper              : [52.882,-118.07880],
+    banff               : [51.17836,-115.57077],
+    calgary             : [51.04532,-114.05810],
+    revelstoke          : [50.99867,-118.19439],
+    kelowna             : [49.88795,-119.49601],
+    osoyoos             : [49.02947,-119.45171],
+    richmond            : [49.19168,-123.11442]
+};
+
 var map;
 
 window.addEvent('domready', function(j) {
 
-    var geoCities = {
-        vancouver           : [49.290504,-123.117655],
-        vancouverBeaverLake : [49.30520,-123.13878],
-        tsawwassen          : [49.00659,-123.13288],
-        victoria            : [48.42373,-123.35449],
-        tofino              : [49.15243,-125.90249],
-        nanaimo             : [49.16664,-123.93497],
-        horseshoeBay        : [49.37579,-123.27160],
-        whistler            : [50.11632,-122.95736],
-        clearwater          : [51.64102,-120.02728],
-        jasper              : [52.882,-118.07880],
-        banff               : [51.17836,-115.57077],
-        calgary             : [51.04532,-114.05810],
-        revelstoke          : [50.99867,-118.19439],
-        kelowna             : [49.88795,-119.49601],
-        osoyoos             : [49.02947,-119.45171],
-        richmond            : [49.19168,-123.11442]
-    };
 
     // https://developers.google.com/maps/documentation/javascript/examples/
 
@@ -170,30 +172,34 @@ window.addEvent('domready', function(j) {
 
     // ShootOnSight each photos
 
-    $$('.photo').addEvent('click', function(e) {
+    /*$$('.photo').addEvent('click', function(e) {
         var city = this.get('data-city');
         if(city && geoCities.hasOwnProperty(city)) {
             var latLng = new google.maps.LatLng(geoCities[city][0],geoCities[city][1]);
             map.panTo(latLng);
         }
+    });*/
+
+
+    // $$('.photo').each(function(photo) {
+    //     shootOnSight(photo, panMap);
+    // });
+
+
+    $(window).addEvent('scroll',function(e){
+        findTheStar($$('.photo'), panMap);
     });
-
-
-    $$('.photo').each(function(photo) {
-        shootOnSight(photo, panMap);
-    });
-
-
-    function panMap() {
-        var city = this.get('data-city');
-        if(city && geoCities.hasOwnProperty(city)) {
-            var latLng = new google.maps.LatLng(geoCities[city][0],geoCities[city][1]);
-            map.panTo(latLng);
-        }
-    }
 
 
 });
+
+function panMap() {
+    var city = this.get('data-city');
+    if(city && geoCities.hasOwnProperty(city)) {
+        var latLng = new google.maps.LatLng(geoCities[city][0],geoCities[city][1]);
+        map.panTo(latLng);
+    }
+}
 
 
 // functions
@@ -219,5 +225,26 @@ function shootOnSight(el, callback) {
                 el.store('shot', true);
             }
         });
+    }
+}
+
+// Find the most focus element in the viewport
+
+function findTheStar(els, callback) {
+    var theStar;
+
+    // étudions tous les elements
+    els.each(function(el){
+        sight = window.getScroll().y + window.getHeight();
+        heartPrey = el.getPosition().y + (el.getHeight()/1.2);
+        if(sight >= heartPrey && heartPrey > window.getScroll().y ) {
+            theStar = el;
+        }
+
+    });
+    if(theStar) {
+        var callbackFn = callback.bind(theStar);
+        callbackFn();
+
     }
 }
