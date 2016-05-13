61e718267d7d7a3f312d25b5c88c25ea8e62198e
Remy | Colorz <remy@colorz.fr>
2016-05-13 14:23:23 +0200

    NS scripts

---
+++ northernscotland/scripts.js
@@ -0,0 +1,80 @@
+var $window,
+    count,
+    images,
+    imagesList,
+    overlay,
+    partHeight;
+
+$(document).on('ready', function(){
+    $window = $(window);
+
+    count = 20;
+    images = $('.images');
+    overlay = $('.overlay');
+
+    for (var i = 2; i <= count; i++) {
+        images.append( '<div class="image fade"><img src="images/photo ('+i+').jpg"></div>' );
+    }
+
+    documentHeight = images.outerHeight();
+    partHeight = documentHeight / count;
+
+    imagesList = images.find('.image');
+
+
+    $window.on("scroll resize", check_if_in_view);
+    check_if_in_view();
+});
+
+
+function check_if_in_view() {
+    var vwHeight = $window.height(),
+        scroll = $window.scrollTop(),
+        bottom = scroll + vwHeight,
+        vwMiddleY = bottom - (vwHeight / 2);
+
+    var currentPart = Math.ceil( vwMiddleY / partHeight );
+    var currentPartIndex = currentPart-1;
+    currentPartStart = (currentPart-1) * partHeight;
+    currentPartOneThird = currentPartStart + (partHeight/3);
+    currentPartTwoThird = currentPartStart + ( 2 * (partHeight/3) );
+    currentPartEnd = currentPartStart + partHeight;
+
+    // display the current
+    // imagesList.removeClass('in');
+    // imagesList.eq( (currentPart-1) ).addClass('in');
+
+    // overlay.addClass('in');
+
+
+    if( scroll < (partHeight/2) ) {
+        // on est au debut
+        imagesList.removeClass('in').removeClass('semi-in');
+        imagesList.eq( (currentPart-1) ).addClass('in');
+
+    } else {
+
+        if( vwMiddleY >= currentPartStart && vwMiddleY < currentPartOneThird ) {
+            console.log( 'between '+(currentPart-1)+' and '+ currentPart );
+
+            imagesList.removeClass('in').removeClass('semi-in');
+            imagesList.eq( (currentPartIndex) ).addClass('semi-in');
+            imagesList.eq( (currentPartIndex-1) ).addClass('semi-in');
+        }
+        else if( vwMiddleY > currentPartTwoThird && vwMiddleY <= currentPartEnd ) {
+            console.log( 'between '+(currentPartIndex)+' and '+(currentPartIndex+1) );
+
+            imagesList.removeClass('in').removeClass('semi-in');
+            imagesList.eq( (currentPartIndex) ).addClass('semi-in');
+            imagesList.eq( (currentPartIndex+1) ).addClass('semi-in');
+        }
+        else if( vwMiddleY >= currentPartOneThird && vwMiddleY <= currentPartTwoThird ) {
+            console.log('safe zone part '+currentPartIndex);
+            // overlay.removeClass('in');
+
+
+            imagesList.removeClass('in').removeClass('semi-in');
+            imagesList.eq( (currentPartIndex) ).addClass('in');
+        }
+    }
+}
